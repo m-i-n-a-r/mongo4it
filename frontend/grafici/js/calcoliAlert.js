db84bd65987923e4fbe93cffdfb3b0bd45c1c608
minar <minar.tastic@gmail.com>
2022-03-18 00:10:53 +0100

    Better UI, still needs work

--- frontend/grafici/js/calcoliAlert.js
+++ frontend/grafici/js/calcoliAlert.js
@@ -11,50 +11,121 @@ const valoreSogliaFeriale = 2.07
 var mediaIncidentiRomaFeriali = Math.round(totIncidentiFeriali / giorniFeriali, 0)
 var mediaIncidentiRomaFestivi = Math.round(totIncidentiFestivi / giorniFestivi, 0)
 
-App = { 
+App = {
 
-    getCalcoloAlert : async (lat, long, flgFeriale, orario, clima) => {
-alert('lat:'+lat+' long:'+long+' flgFeriale:'+flgFeriale+'orario:'+orario+' clima:'+clima);
+    getCalcoloAlert: async (lat, long, flgFeriale, orario, clima, anno) => {
+        //alert('lat:' + lat + ' long:' + long + ' flgFeriale:' + flgFeriale + 'orario:' + orario + ' clima:' + clima + ' anno:' + anno);
 
+        if (anno != null) {
+            anno = parseInt(anno)
+        }
         orario = parseInt(orario);
-        if (flgFeriale) {
-            var queryMediaRoma = JSON.stringify({
-                "collection": "Incidente_Geo_Distinct",
-                "database": "Hackaton",
-                "dataSource": "demo",
-                "filter": { $and : [{"geoPoint" :
-                    { $near :
-                    {
-                        $geometry : {
-                            type : "Point" ,
-                            coordinates : [lat, long] },
-                        $maxDistance : 1000
+        // Se Anno = All 
+        if (anno == null) {
+            if (flgFeriale) {
+                var queryMediaRoma = JSON.stringify({
+                    "collection": "Incidente_Geo_Distinct",
+                    "database": "Hackaton",
+                    "dataSource": "demo",
+                    "filter": {
+                        $and: [{
+                            "geoPoint":
+                            {
+                                $near:
+                                {
+                                    $geometry: {
+                                        type: "Point",
+                                        coordinates: [lat, long]
+                                    },
+                                    $maxDistance: 1000
+                                }
+                            }
+                        }, { "dayOfWeek": { $gt: 1, $lt: 7 } },
+                        { ora: { $gte: orario - 1, $lte: orario + 1 } },
+                        { CondizioneAtmosferica: clima }
+                        ]
                     }
-                    }}, {"dayOfWeek": { $gt : 1 , $lt : 7}}, 
-                    {ora: { $gte : orario - 1, $lte : orario + 1 }},
-                    {CondizioneAtmosferica: clima}
-                ]
-                }
-            });
-        } else {
-            var queryMediaRoma = JSON.stringify({
-                "collection": "Incidente_Geo_Distinct",
-                "database": "Hackaton",
-                "dataSource": "demo",
-                "filter": { $and : [{geoPoint :
-                    { $near :
-                    {
-                        $geometry : {
-                            type : "Point" ,
-                            coordinates : [lat, long] },
-                        $maxDistance : 1000
+                });
+            } else {
+                var queryMediaRoma = JSON.stringify({
+                    "collection": "Incidente_Geo_Distinct",
+                    "database": "Hackaton",
+                    "dataSource": "demo",
+                    "filter": {
+                        $and: [{
+                            geoPoint:
+                            {
+                                $near:
+                                {
+                                    $geometry: {
+                                        type: "Point",
+                                        coordinates: [lat, long]
+                                    },
+                                    $maxDistance: 1000
+                                }
+                            }
+                        }, { dayOfWeek: { $eq: 1, $eq: 7 } },
+                        { ora: { $gte: orario - 1, $lte: orario + 1 } },
+                        { CondizioneAtmosferica: clima }
+                        ]
                     }
-                    }}, {dayOfWeek: { $eq : 1 , $eq : 7}},  
-                    {ora: { $gte : orario - 1, $lte : orario + 1 }},
-                    {CondizioneAtmosferica: clima}
-                ]
-                }
-            });
+                });
+            }
+        }
+        // Se Anno è valorizzato 
+        else {
+            if (flgFeriale) {
+                var queryMediaRoma = JSON.stringify({
+                    "collection": "Incidente_Geo_Distinct",
+                    "database": "Hackaton",
+                    "dataSource": "demo",
+                    "filter": {
+                        $and: [{
+                            "geoPoint":
+                            {
+                                $near:
+                                {
+                                    $geometry: {
+                                        type: "Point",
+                                        coordinates: [lat, long]
+                                    },
+                                    $maxDistance: 1000
+                                }
+                            }
+                        }, { "dayOfWeek": { $gt: 1, $lt: 7 } },
+                        { ora: { $gte: orario - 1, $lte: orario + 1 } },
+                        { CondizioneAtmosferica: clima },
+                        { "data": { $gte: new Date(anno, 0, 1), $lte: new Date(anno, 11, 31) } }
+
+                        ]
+                    }
+                });
+            } else {
+                var queryMediaRoma = JSON.stringify({
+                    "collection": "Incidente_Geo_Distinct",
+                    "database": "Hackaton",
+                    "dataSource": "demo",
+                    "filter": {
+                        $and: [{
+                            geoPoint:
+                            {
+                                $near:
+                                {
+                                    $geometry: {
+                                        type: "Point",
+                                        coordinates: [lat, long]
+                                    },
+                                    $maxDistance: 1000
+                                }
+                            }
+                        }, { dayOfWeek: { $eq: 1, $eq: 7 } },
+                        { ora: { $gte: orario - 1, $lte: orario + 1 } },
+                        { CondizioneAtmosferica: clima },
+                        { "data": { $gte: new Date(anno, 0, 1), $lte: new Date(anno, 11, 31) } }
+                        ]
+                    }
+                });
+            }
         }
 
         var queryChartMapConfig = {
@@ -65,24 +136,24 @@ alert('lat:'+lat+' long:'+long+' flgFeriale:'+flgFeriale+'orario:'+orario+' clim
                 'Access-Control-Request-Headers': '*',
                 'api-key': '5ZeDsX1tgu7nVn7kJV74XfGcOiN7UMjbiNX36hmEDfataprwx00krrZGrWWw0kjQ'
             },
-            data : queryMediaRoma
+            data: queryMediaRoma
         };
 
         let esito = "OK"
 
         await axios(queryChartMapConfig)
-        .then(function (response) {
-            // let numeroDocuments = response.data.documents[0].numTotaleIncidenti;
-            // return numeroDocuments
-            console.log(JSON.stringify(response.data.documents));
-            if(response.data.documents.length > valoreSogliaFestivi ) {
-                esito = "Hai più probabilità di fare incidenti ("+response.data.documents.length+")";
-            } else {
-                esito = "Vai tranquillo, rischio minore della media"
-            } 
-        }).catch(function (error) {
-            console.log(error);
-        });
+            .then(function (response) {
+                // let numeroDocuments = response.data.documents[0].numTotaleIncidenti;
+                // return numeroDocuments
+                console.log(JSON.stringify(response.data.documents));
+                if (response.data.documents.length > valoreSogliaFestivi) {
+                    esito = "<strong>Hai più probabilità di fare incidenti (" + response.data.documents.length + ")</strong>";
+                } else {
+                    esito = "<strong>Vai tranquillo, rischio minore della media</strong>"
+                }
+            }).catch(function (error) {
+                console.log(error);
+            });
 
         return esito
     },
--- frontend/grafici/js/chart.js
+++ frontend/grafici/js/chart.js
@@ -1,141 +1,152 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiYXBlMTYzIiwiYSI6ImNsMHRydnl3YjBvaDQzam5ta3N1eXcwemEifQ.XtSoUweJyOjv-kdolaHmKQ';
 const map = new mapboxgl.Map({
-    container: 'map', // container element id
-    style: 'mapbox://styles/mapbox/light-v10',
-    center: [12.4963655, 41.9027835], // initial map center in [lon, lat]
-    zoom: 10
-  });
-  var lon = 12.4963655;
-  var lat = 41.9027835;
-function error(){
+  container: 'map', // container element id
+  style: 'mapbox://styles/mapbox/light-v10',
+  center: [12.4963655, 41.9027835], // initial map center in [lon, lat]
+  zoom: 10
+});
+var lon = 12.4963655;
+var lat = 41.9027835;
+function error() {
 
-  
-  maximumLon = 12.55 
-  minimumLon = 12.33 
-  maximumLat = 42 
-  minimumLat = 41.779 
+
+  maximumLon = 12.55
+  minimumLon = 12.33
+  maximumLat = 42
+  minimumLat = 41.779
   position = {};
-  
-  position.coords={};
+
+  position.coords = {};
   position.coords.latitude = Math.random() * (maximumLat - minimumLat) + minimumLat;
-  position.coords.longitude = Math.random() * (maximumLon - minimumLon)+ minimumLon ;
+  position.coords.longitude = Math.random() * (maximumLon - minimumLon) + minimumLon;
   mia_posizione(position)
 }
-function GetSelectedValue(selectItem)
-{
-    var index = document.getElementById(selectItem).selectedIndex;
+function GetSelectedValue(selectItem) {
+  var index = document.getElementById(selectItem).selectedIndex;
   return document.getElementById(selectItem).value;
 }
 
-function rischio(){
+function rischio() {
   ora = document.getElementById('slider').value;
   giorno = document.getElementById('filters').value;
 
+  var listaAnni = document.getElementsByName("toggle")
+  var anno
+  var i;
+  for (i = 0; i < listaAnni.length; i++) {
+    if (listaAnni[i].checked) {
+      anno = listaAnni[i].value;
+    }
+  }
+
+  //alert('anno' + anno)
+  if (!anno || anno == "all") {
+    anno = null
+  }
 
 
-
-
-  meteoS =GetSelectedValue('meteoS');
-  if(!meteoS || meteoS == "All"){
+  meteoS = GetSelectedValue('meteoS');
+  if (!meteoS || meteoS == "All") {
     meteoS = "Sereno"
   }
 
+
   giornoBool = giorno == 'Feriali';
-  
-  App.getCalcoloAlert(lon, lat, giornoBool, ora, meteoS).then((data) => {
+
+  App.getCalcoloAlert(lon, lat, giornoBool, ora, meteoS, anno).then((data) => {
     // const allIncidenti = data.documents
     console.log("Prova", data)
-     document.getElementById('pippo').innerHTML=data;
+    document.getElementById('pippo').innerHTML = data;
     // const totaleIncidenti = allIncidenti.length;
     // console.log("Prova: ", allIncidenti)
-})
+  })
 }
 
 
-  function mia_posizione(position) {
-     lat = position.coords.latitude;
-     lon = position.coords.longitude;
+function mia_posizione(position) {
+  lat = position.coords.latitude;
+  lon = position.coords.longitude;
 
 
-     rischio()
-
-
-
-  
+  rischio()
 
 
 
-    const geojson = {
-      'type': 'FeatureCollection',
-      'features': [
+
+
+
+
+  const geojson = {
+    'type': 'FeatureCollection',
+    'features': [
       {
-      'type': 'Feature',
-      'properties': {
-      'message': 'Baz',
-      'iconSize': [40, 40]
-      },
-      'geometry': {
-      'type': 'Point',
-      'coordinates': [lon, lat]
+        'type': 'Feature',
+        'properties': {
+          'message': 'Baz',
+          'iconSize': [40, 40]
+        },
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [lon, lat]
+        }
       }
-      }
-      ]
-      };
-// Add markers to the map.
-for (const marker of geojson.features) {
-  // Create a DOM element for each marker.
-  const el = document.getElementById('marker');
-  const width = marker.properties.iconSize[0];
-  const height = marker.properties.iconSize[1];
-  el.className = 'marker';
-  el.style.backgroundImage = `url(../images/mongo.png)`;
-  el.style.backgroundSize = '100%';
-   
-  el.addEventListener('click', () => {
-  window.alert(marker.properties.message);
-  });
-   
+    ]
+  };
   // Add markers to the map.
-  new mapboxgl.Marker(el)
-  .setLngLat(marker.geometry.coordinates)
-  .addTo(map);
+  for (const marker of geojson.features) {
+    // Create a DOM element for each marker.
+    const el = document.getElementById('marker');
+    const width = marker.properties.iconSize[0];
+    const height = marker.properties.iconSize[1];
+    el.className = 'marker';
+    el.style.backgroundImage = `url(../images/car.png)`;
+    el.style.backgroundSize = '100%';
+
+    el.addEventListener('click', () => {
+      //window.alert(marker.properties.message);
+    });
+
+    // Add markers to the map.
+    new mapboxgl.Marker(el)
+      .setLngLat(marker.geometry.coordinates)
+      .addTo(map);
   }
 
-  }
-
- 
+}
 
 
 
 
 
-  var queryChartMap = JSON.stringify({
-    "collection": "Incidente_Geo_Distinct",
-    "database": "Hackaton",
-    "dataSource": "demo",
-    "filter" : {},
-    "projection": {
-      "geometry": "$geoPoint",
-      "properties": {
-        "Coinvolti": {$sum:["$NUM_FERITI","$NUM_MORTI"]},
-        "Ora": "$ora",
-        "Giorno": "$dayOfWeek",
-        "Anno": {"$year":"$data"},
-        "Meteo": "$CondizioneAtmosferica"
-      }
-    },
-    "limit":50000
+
+
+var queryChartMap = JSON.stringify({
+  "collection": "Incidente_Geo_Distinct",
+  "database": "Hackaton",
+  "dataSource": "demo",
+  "filter": {},
+  "projection": {
+    "geometry": "$geoPoint",
+    "properties": {
+      "Coinvolti": { $sum: ["$NUM_FERITI", "$NUM_MORTI"] },
+      "Ora": "$ora",
+      "Giorno": "$dayOfWeek",
+      "Anno": { "$year": "$data" },
+      "Meteo": "$CondizioneAtmosferica"
+    }
+  },
+  "limit": 50000
 
 });
 var queryChartMapConfig = {
-    method: 'post',
-    url: 'https://data.mongodb-api.com/app/data-bcvqg/endpoint/data/beta/action/find',
-    headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Request-Headers': '*',
-        'api-key': '5ZeDsX1tgu7nVn7kJV74XfGcOiN7UMjbiNX36hmEDfataprwx00krrZGrWWw0kjQ'
-    },
-    data : queryChartMap
+  method: 'post',
+  url: 'https://data.mongodb-api.com/app/data-bcvqg/endpoint/data/beta/action/find',
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Request-Headers': '*',
+    'api-key': '5ZeDsX1tgu7nVn7kJV74XfGcOiN7UMjbiNX36hmEDfataprwx00krrZGrWWw0kjQ'
+  },
+  data: queryChartMap
 };
 
 
@@ -144,13 +155,13 @@ var queryChartLayer = JSON.stringify({
   "collection": "Municipi",
   "database": "Hackaton",
   "dataSource": "demo",
-  "filter" : {},
+  "filter": {},
   "projection": {
     "geometry": 1,
     "properties": {
-      "mun": {  '$toInt':{'$substr': [ '$id', 20, 2 ] } }
+      "mun": { '$toInt': { '$substr': ['$id', 20, 2] } }
     }
-    
+
   }
 
 
@@ -159,158 +170,168 @@ var queryChartLayerConfig = {
   method: 'post',
   url: 'https://data.mongodb-api.com/app/data-bcvqg/endpoint/data/beta/action/find',
   headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Request-Headers': '*',
-      'api-key': '5ZeDsX1tgu7nVn7kJV74XfGcOiN7UMjbiNX36hmEDfataprwx00krrZGrWWw0kjQ'
+    'Content-Type': 'application/json',
+    'Access-Control-Request-Headers': '*',
+    'api-key': '5ZeDsX1tgu7nVn7kJV74XfGcOiN7UMjbiNX36hmEDfataprwx00krrZGrWWw0kjQ'
   },
-  data : queryChartLayer
+  data: queryChartLayer
 };
 
 axios(queryChartLayerConfig)
-    .then(function (response) {
-      console.log(JSON.stringify(response.data.documents.length));
-      createGraphMun(response.data);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+  .then(function (response) {
+    console.log(JSON.stringify(response.data.documents.length));
+    createGraphMun(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
-function createGraph(data){
-    console.log('dentro createGraph');
-    filterHour = ['==', ['number', ['get', 'Ora']], 12];
-    filterDay = ['!=', ['number', ['get', 'Giorno']], 0];
-    filterYear = ['!=', ['number', ['get', 'Anno']], 0];
-    filterMeteo = ['!=', ['string', ['get', 'Meteo']], 'pippo'];
-    map.addLayer({
-      id: 'collisions',
-      type: 'circle',
-      source: {
-        type: 'geojson',
-        data: 
-            {
-                "type": "FeatureCollection",
-                "features": data.documents
-        }
-      },
-      paint: {
-        'circle-radius': [
-          'interpolate',
-          ['linear'],
-          ['number', ['get', 'Coinvolti']],
-          0,
-          4,
-          5,
-          24
-        ],
-        'circle-color': [
-          'interpolate',
-          ['linear'],
-          ['number', ['get', 'Coinvolti']],
-          0,
-          '#2DC4B2',
-          1,
-          '#3BB3C3',
-          2,
-          '#669EC4',
-          3,
-          '#8B88B6',
-          4,
-          '#A2719B',
-          5,
-          '#AA5E79'
-        ],
-        'circle-opacity': 0.8
-      },
-      filter: ['all', filterHour, filterDay, filterYear,filterMeteo]
-    });
+function createGraph(data) {
+  console.log('dentro createGraph');
+  filterHour = ['==', ['number', ['get', 'Ora']], 12];
+  filterDay = ['!=', ['number', ['get', 'Giorno']], 0];
+  filterYear = ['!=', ['number', ['get', 'Anno']], 0];
+  filterMeteo = ['!=', ['string', ['get', 'Meteo']], 'pippo'];
+  map.addLayer({
+    id: 'collisions',
+    type: 'circle',
+    source: {
+      type: 'geojson',
+      data:
+      {
+        "type": "FeatureCollection",
+        "features": data.documents
+      }
+    },
+    paint: {
+      'circle-radius': [
+        'interpolate',
+        ['linear'],
+        ['number', ['get', 'Coinvolti']],
+        0,
+        4,
+        5,
+        24
+      ],
+      'circle-color': [
+        'interpolate',
+        ['linear'],
+        ['number', ['get', 'Coinvolti']],
+        0,
+        '#2DC4B2',
+        1,
+        '#3BB3C3',
+        2,
+        '#669EC4',
+        3,
+        '#8B88B6',
+        4,
+        '#A2719B',
+        5,
+        '#AA5E79'
+      ],
+      'circle-opacity': 0.7
+    },
+    filter: ['all', filterHour, filterDay, filterYear, filterMeteo]
+  });
 
-    document.getElementById('slider').addEventListener('input', (event) => {
-      rischio()
-      const hour = parseInt(event.target.value);
-      // update the map
+  document.getElementById('slider').addEventListener('input', (event) => {
+    rischio()
+    const hour = parseInt(event.target.value);
+    // update the map
+    filterHour = ['==', ['number', ['get', 'Ora']], hour];
+    map.setFilter('collisions', ['all', filterHour, filterDay, filterYear, filterMeteo]);
+
+    // converting 0-23 hour to AMPM format
+    const ampm = hour >= 12 ? 'PM' : 'AM';
+    const hour12 = hour % 12 ? hour % 12 : 12;
+
+    // update text in the UI
+    document.getElementById('active-hour').innerText = hour12 + ampm;
+  });
+  document.getElementById('hourNone').addEventListener('click', (event) => {
+    rischio()
+    const checked = event.target.checked;
+    // update the map
+    if (checked) {
+      filterHour = ['!=', ['number', ['get', 'Ora']], 25];
+    }
+    else {
+      hour = parseInt(document.getElementById('slider').value);
       filterHour = ['==', ['number', ['get', 'Ora']], hour];
-      map.setFilter('collisions', ['all', filterHour, filterDay, filterYear,filterMeteo]);
-    
-      // converting 0-23 hour to AMPM format
-      const ampm = hour >= 12 ? 'PM' : 'AM';
-      const hour12 = hour % 12 ? hour % 12 : 12;
-    
-      // update text in the UI
-      document.getElementById('active-hour').innerText = hour12 + ampm;
-    });
-    document.getElementById('hourNone').addEventListener('click', (event) => {
-      rischio()
-      const checked = event.target.checked;
-      // update the map
-      if(checked){
-        filterHour = ['!=', ['number', ['get', 'Ora']], 25];
-      }
-      else{
-        hour = parseInt(document.getElementById('slider').value);
-        filterHour = ['==', ['number', ['get', 'Ora']], hour];
-      }
-      
-      map.setFilter('collisions', ['all', filterHour, filterDay, filterYear,filterMeteo]);
-    });
-    document.getElementById('filters').addEventListener('change', (event) => {
-      rischio()
-      const day = event.target.value;
-      if (day === 'all') {
-        filterDay = ['!=', ['number', ['get', 'Giorno']], 0];
-      } else if (day === 'weekday') {
-        filterDay = ['match', ['get', 'Giorno'], [7, 1], false, true];
-      } else if (day === 'weekend') {
-        filterDay = ['match', ['get', 'Giorno'], [7, 1], true, false];
-      } else {
-        console.log('error');
-      }
-      map.setFilter('collisions', ['all', filterHour, filterDay, filterYear,filterMeteo]);
-    });
-    document.getElementById('years').addEventListener('change', (event) => {
-      rischio()
-      year = event.target.value;
-      year = parseInt(year);
-      filterYear = ['==', ['number', ['get', 'Anno']], year];
-      map.setFilter('collisions', ['all', filterHour, filterDay, filterYear]);
-    });
-    document.getElementById('meteoS').addEventListener('change', (event) => {
-      meteo = event.target.value;
-      alert('ciao'+meteo)
-      if(meteo == 'All'){
-        filterMeteo = ['!=', ['string', ['get', 'Meteo']], "pippo"];
-      }
-      else{
-        filterMeteo = ['==', ['string', ['get', 'Meteo']], meteo];
-        alert(filterMeteo)
-      }
-      
-      map.setFilter('collisions', ['all', filterHour, filterDay, filterYear,filterMeteo]);
-    });
-    
-    console.log('fine map');
-
-
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(mia_posizione, error);
-    }else{
-      alert('La geo-localizzazione NON è possibile');
     }
 
+    map.setFilter('collisions', ['all', filterHour, filterDay, filterYear, filterMeteo]);
+  });
+  document.getElementById('filters').addEventListener('change', (event) => {
+    rischio()
+    const day = event.target.value;
+    if (day === 'all') {
+      filterDay = ['!=', ['number', ['get', 'Giorno']], 0];
+    } else if (day === 'weekday') {
+      filterDay = ['match', ['get', 'Giorno'], [7, 1], false, true];
+    } else if (day === 'weekend') {
+      filterDay = ['match', ['get', 'Giorno'], [7, 1], true, false];
+    } else {
+      console.log('error');
+    }
+    map.setFilter('collisions', ['all', filterHour, filterDay, filterYear, filterMeteo]);
+  });
+  document.getElementById('years').addEventListener('change', (event) => {
+
+    rischio()
+    //Stef
+    document.getElementById(event.target.value).checked = true;
+    //FINE STEF
+    year = event.target.value;
+
+    if (year === 'all') {
+      filterYear = ['!=', ['number', ['get', 'Anno']], 0];
+    }
+    else {
+      year = parseInt(year);
+      filterYear = ['==', ['number', ['get', 'Anno']], year];
+    }
+    map.setFilter('collisions', ['all', filterHour, filterDay, filterYear, filterMeteo]);
+  });
+  document.getElementById('meteoS').addEventListener('change', (event) => {
+    meteo = event.target.value;
+    //alert('ciao' + meteo)
+    if (meteo == 'All') {
+      filterMeteo = ['!=', ['string', ['get', 'Meteo']], "pippo"];
+    }
+    else {
+      filterMeteo = ['==', ['string', ['get', 'Meteo']], meteo];
+      //alert(filterMeteo)
+    }
+
+    map.setFilter('collisions', ['all', filterHour, filterDay, filterYear, filterMeteo]);
+  });
+
+  console.log('fine map');
+
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(mia_posizione, error);
+  } else {
+    alert('La geo-localizzazione NON è possibile');
+  }
 
 
 
 
 
-    
+
+
 
 
 
 
 }
 
-function createGraphMun(data){
+function createGraphMun(data) {
   console.log(JSON.stringify(data.documents.length));
-var dati = data.documents;
+  var dati = data.documents;
 
 
   map.addLayer(
@@ -334,9 +355,9 @@ var dati = data.documents;
             ['linear'],
             ['get', 'mun'],
             1,
-            'rgba(222,235,247,1)',
+            '#049A41',
             16,
-            'rgba(49,130,189,1)'
+            '#112E2C'
           ],
           'rgba(255, 255, 255, 0)'
         ],
@@ -345,13 +366,13 @@ var dati = data.documents;
     }
   );
 
-  
-axios(queryChartMapConfig)
-.then(function (response) {
-    console.log(JSON.stringify(response.data.documents));
-    createGraph(response.data);
-})
-.catch(function (error) {
-    console.log(error);
-});
+
+  axios(queryChartMapConfig)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data.documents));
+      createGraph(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
